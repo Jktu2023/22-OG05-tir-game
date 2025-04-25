abd6a0e650073e2f901dec709e5c490882c4b772
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-04-25 16:26:00 +0700

    Ready 2
    готово 2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ BLACK = (0,0,0)
 WIDTH = 800  # ширина игрового окна
 HEIGHT = 600 # высота игрового окна
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) #  установка параметров окна
 pygame.display.set_caption('Игра Тир')
 image_target = pygame.image.load('target.png') # Загружаем изображение
 # размеры мишени
@@ -24,20 +24,28 @@ target_y = random.randint(0, HEIGHT - T_HEIGHT)
 
 running = True  # Зададим переменную running → пропишем, что она равна True
 while running:  # → создадим цикл while
-    screen.fill(BLACK)
-    for event in pygame.event.get():
+    # Ввод процесса (события)
+    # Обновление
+    # Визуализация (сборка)
+    for event in pygame.event.get(): # если так то выход
         if event.type == pygame.QUIT:
             running = False
-    if event.type == pygame.MOUSEBUTTONDOWN:
+    if event.type == pygame.MOUSEBUTTONDOWN: # Ввод процесса (события)
         mouse_x, mouse_y = pygame.mouse.get_pos() # сохраняем координаты курсора мыши, куда мы кликнули
         # находится ли координата мыши mouse_ в пределах координаты мишени target_ и размером этой мишени (target_ + T_РАЗМЕРМИШЕНИ)
         if target_x < mouse_x < (target_x + T_WIDTH) and target_y < mouse_y < (target_y + T_HEIGHT):
             # новые рандомные координаты мишени - мишень меняет положение в окне
+            # Обновление
             target_x = random.randint(0, WIDTH - T_WIDTH)
             target_y = random.randint(0, HEIGHT - T_HEIGHT)
 
-    screen.blit(image_target, (target_x, target_y))
-    pygame.display.update()
+    #  рендеринг, Визуализация (сборка)
+    screen.fill(BLACK)# заливаем экран черным
+    screen.blit(image_target, (target_x, target_y))  #  рисуем мишень в новом месте, переворачиваем видеопамять лицом к нам
+    pygame.display.update()  #  обновляем
+
+    # Контроль ФПС # держим игорвой цикл на правильной скорости
+    clock.tick(fps) # нужно убедиться, что настройка FPS контролирует скорость игры.
 
 pygame.quit()  # выходим так
 sys.exit() # и эдак
